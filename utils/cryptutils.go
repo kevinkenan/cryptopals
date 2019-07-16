0c7a1c43e160c56e4bc725f54c4938ad2004c53e
Kevin Kenan <kevin@kevinkenan.com>
2019-07-16 06:30:23 -0700

    Cleaned up Mersenne Twister

--- utils/cryptutils.go
+++ utils/cryptutils.go
@@ -451,100 +451,91 @@ func min(a, b int) int {
 }
 
 // Mersenne Twister (MT19937)
-func RandomGen(seed uint32, ch chan uint32) {
-	// Values needed by the algorithm.
-	u := uint32(11)
-	d := uint32(0xFFFFFFFF)
-	s := uint32(7)
-	b := uint32(0x9D2C5680)
-	t := uint32(15)
-	c := uint32(0xEFC60000)
-	l := uint32(18)
 
+// Constants
+const mt_w = uint32(32)
+const mt_n = uint32(624)
+const mt_m = uint32(397)
+const mt_r = uint32(31)
+const mt_a = uint32(0x9908B0DF)
+const mt_u = uint32(11)
+const mt_d = uint32(0xFFFFFFFF)
+const mt_s = uint32(7)
+const mt_b = uint32(0x9D2C5680)
+const mt_t = uint32(15)
+const mt_c = uint32(0xEFC60000)
+const mt_l = uint32(18)
+const mt_f = uint32(1812433253)
+const lowerMask = uint32((1 << mt_r) - 1) // 0x7fffffff
+const upperMask = uint32(^lowerMask)      // 0x80000000
+
+type mt19937 struct {
+	initialized bool
+	index       uint32   // the index of the next random number to be returned
+	state       []uint32 // the current set of random numbers
+}
+
+func RandomGen(seed uint32, ch chan uint32) {
 	// Initialization
 	var mt mt19937
 	mt.init(seed)
 
 	for {
-		_, err := mt.twist()
-		if err != nil {
-			fmt.Println("ERROR:", err)
-			return
+		if mt.index == mt_n {
+			_, err := mt.twist()
+			if err != nil {
+				fmt.Println("ERROR:", err)
+				return
+			}
 		}
 
-		y := mt.getNextStateValue()
-		y = y ^ ((y >> u) & d)
-		y = y ^ ((y << s) & b)
-		y = y ^ ((y << t) & c)
-		y = y ^ (y >> l)
+		y := mt.state[mt.index]
+		y = y ^ ((y >> mt_u) & mt_d)
+		y = y ^ ((y << mt_s) & mt_b)
+		y = y ^ ((y << mt_t) & mt_c)
+		y = y ^ (y >> mt_l)
+		mt.index++
 
 		ch <- y
 	}
 }
 
-type mt19937 struct {
-	initialized bool
-	n           uint32
-	index       uint32
-	state       []uint32
-}
-
-func (mt *mt19937) getNextStateValue() uint32 {
-	v := mt.state[mt.index]
-	// The twist function periodically resets mt.index back to zero so we
-	// don't run into an out of range issue.
-	mt.index++
-	return v
-}
-
+// Initialize the state.
 func (mt *mt19937) init(seed uint32) {
 	if mt.initialized {
 		return
 	}
 
-	// Values needed by the algorithm.
-	w := uint32(32)
-	f := uint32(1812433253)
-
-	// Initialize the state.
-	mt.n = uint32(624)
-	state := make([]uint32, mt.n)
+	state := make([]uint32, mt_n)
 	state[0] = seed
-	for i := uint32(1); i < mt.n; i++ {
-		state[i] = (f*(state[i-1]^(state[i-1]>>(w-2))) + i)
+	for i := uint32(1); i < mt_n; i++ {
+		state[i] = (mt_f*(state[i-1]^(state[i-1]>>(mt_w-2))) + i)
 	}
 	mt.state = state
 
-	// Index points to the last element of the state.
-	mt.index = mt.n
+	// Point the index to the last element of the state so that
+	// the twist is called before returning any values.
+	mt.index = mt_n
 
 	// Mark it as initialized.
 	mt.initialized = true
 }
 
+// Populate the state array with a new set of values.
 func (mt *mt19937) twist() (uint32, error) {
 	if !mt.initialized {
 		return 0, errors.New("Generator was not seeded")
 	}
 
-	// Values needed by the algorithm.
-	m := uint32(397)
-	r := uint32(31)
-	a := uint32(0x9908B0DF)
-	lowerMask := uint32((1 << r) - 1) // 0x7fffffff
-	upperMask := uint32(^lowerMask)   // 0x80000000
-
-	if mt.index == mt.n {
-		for i := uint32(0); i < mt.n; i++ {
-			x := (mt.state[i] & upperMask) + (mt.state[(i+1)%mt.n] & lowerMask)
-			xA := x >> 1
-			if (x % 2) != 0 { // lowest bit of x is 1
-				xA = xA ^ a
-			}
-			mt.state[i] = mt.state[(i+m)%mt.n] ^ xA
+	for i := uint32(0); i < mt_n; i++ {
+		x := (mt.state[i] & upperMask) + (mt.state[(i+1)%mt_n] & lowerMask)
+		xA := x >> 1
+		if (x % 2) != 0 { // lowest bit of x is 1
+			xA = xA ^ mt_a
 		}
-		mt.index = 0
+		mt.state[i] = mt.state[(i+mt_m)%mt_n] ^ xA
 	}
+	mt.index = 0
 
 	return mt.index, nil
 }
